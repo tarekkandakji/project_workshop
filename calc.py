dbbac36367b96333bcf273198ac4aeacbf7c8f56
tarekkandakji <tarek.kandakji@ttu.edu>
2017-11-11 15:30:58 -0600

    Add something to the text just to excersise

--- calc.py
+++ calc.py
@@ -1,3 +1,3 @@
 def wind_speed(u, v, w):
-    """Calculates wind speed from u, v, and w components."""
+    """Calculates wind speed from u, v, and w components. W add later"""
     return sqrt(u * u + v * v * w)
