a3677576ffc07fc9dc515794c45c09bf48c32a4d
tarekkandakji <tarek.kandakji@ttu.edu>
2017-11-11 16:32:13 -0600

    add clearly correct direction calculation

--- calc.py
+++ calc.py
@@ -1,3 +1,5 @@
-def wind_speed(u, v):
-    """Calculates wind speed from u and v components."""
-    return sqrt(u * u + v * v)
+def wind_speed(u, v, w=0):
+    """Calculates wind speed from u, v and w components."""
+    return sqrt(u * u + v * v + w * w)
+def get_wind_direction(u, v):
+    return 90 - atan2(u, v)
